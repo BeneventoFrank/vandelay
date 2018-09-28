5f925b5655aa32b6023289c6793f10522bcb2b49
frankbenevento <fb.mentoslabs@gmail.com>
2018-09-28 08:54:46 -0700

    testing

--- js/index.js
+++ js/index.js
@@ -1,45 +1,32 @@
 
-let menuBtn = document.querySelector('#mainMenu');
-let closeBtn = document.querySelector('#mainMenuClose');
-let menuDiv = document.querySelector('#menuDiv');
-let navMenu = document.querySelector('#navMenu');
-let headerTopNavDiv = document.querySelector('#headerTopNavDiv');
-let activePage = document.querySelector('#selectedPage');
-
-closeBtn.classList.add('hidden');
-navMenu.classList.add('hidden');
 
 
-menuBtn.addEventListener('click',() => {
-    menuDiv.classList.add('header-overlay');
-    menuBtn.classList.add('hidden');
-    closeBtn.classList.remove('hidden');
-    headerTopNavDiv.classList.remove('justCenter');
-    headerTopNavDiv.classList.add('addMargin');
-    navMenu.classList.remove('hidden');
-    activePage.classList.add('selectedPage');
-})
+// let imgDiv = document.querySelector('#main-carousel-img');
+// imgDiv.classList.add(`main-carousel-img-1`);
+// let sec = 0;
+// let imgCounter = 2;
 
-closeBtn.addEventListener('click',() => {
-    menuDiv.classList.remove('header-overlay');
-    menuBtn.classList.remove('hidden');
-    headerTopNavDiv.classList.add('justCenter');
-    headerTopNavDiv.classList.remove('addMargin');
-    closeBtn.classList.add('hidden');
-    navMenu.classList.add('hidden');
-})
+// setInterval(()=>{
+//     if(sec === 4){
+//         for(let i = 0; i<=3;i++){
+//             imgDiv.classList.remove(`main-carousel-img-${i}`);
+//         }
+
+        
+//         TweenMax.to(imgDiv, 1, { x:100 ,  ease:Power2.easeIn });        
+
+//         imgDiv.classList.add(`main-carousel-img-${imgCounter}`);
+//         sec = 0;
+//         imgCounter = imgCounter +1;
+//         if(imgCounter===4){
+//            imgCounter = 1;
+//         }
+//     }
+//     sec = sec +1;
+// },1000)
 
 
-let logo = document.querySelector('#logo');
-TweenMax.to(logo, 1, { x:100 ,  ease:Power2.easeIn });
-
-let sjlogo = document.querySelector('#sjLogo');
-TweenMax.to(sjlogo, 1, { x:100 ,  ease:Power2.easeIn });
-    
-let menu = document.querySelector('#menuDiv');
-TweenMax.to(menu, 1, { y:50 ,  ease:Power2.easeIn } );
-
-let hamburger = document.querySelector('#mainMenu');
-TweenMax.to(hamburger, 1, { x:-100 ,  ease:Power2.easeIn } );
-    
-
+function checkSlide(e){
+    console.log(e);
+}
+window.addEventListener('scroll',checkSlide);
