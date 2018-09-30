dda6d987f8148633700b4094340a5d558b2cb101
frankbenevento <fb.mentoslabs@gmail.com>
2018-09-29 21:11:56 -0700

    testing

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
 
 
 
-    function debounce(func, wait = 20, immediate = true) {
-      var timeout;
-      return function() {
-        var context = this, args = arguments;
-        var later = function() {
-          timeout = null;
-          if (!immediate) func.apply(context, args);
-        };
-        var callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        if (callNow) func.apply(context, args);
-      };
-    };
+    // function debounce(func, wait = 20, immediate = true) {
+    //   var timeout;
+    //   return function() {
+    //     var context = this, args = arguments;
+    //     var later = function() {
+    //       timeout = null;
+    //       if (!immediate) func.apply(context, args);
+    //     };
+    //     var callNow = immediate && !timeout;
+    //     clearTimeout(timeout);
+    //     timeout = setTimeout(later, wait);
+    //     if (callNow) func.apply(context, args);
+    //   };
+    // };
 
     const sliderImages = document.querySelectorAll('.slide-in');
     console.log('sliderImages', sliderImages)
@@ -45,4 +45,4 @@
       });
     }
 
-    window.addEventListener('scroll', debounce(checkSlide)); 
+    window.addEventListener('scroll', checkSlide); 
