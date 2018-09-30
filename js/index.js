be75a6c7b1bc61e809b76b0f4d9124d3be23dce8
frankbenevento <fb.mentoslabs@gmail.com>
2018-09-29 21:30:56 -0700

    testing

--- js/index.js
+++ js/index.js
@@ -4,42 +4,39 @@
     if(!navigator.userAgent.toLowerCase().includes('ipad')){
 
       
-      function debounce(func, wait = 20, immediate = true) {
-        var timeout;
-        return function() {
-          var context = this, args = arguments;
-          var later = function() {
-            timeout = null;
-            if (!immediate) func.apply(context, args);
-          };
-          var callNow = immediate && !timeout;
-          clearTimeout(timeout);
-          timeout = setTimeout(later, wait);
-          if (callNow) func.apply(context, args);
-        };
-      };
+      // function debounce(func, wait = 20, immediate = true) {
+      //   var timeout;
+      //   return function() {
+      //     var context = this, args = arguments;
+      //     var later = function() {
+      //       timeout = null;
+      //       if (!immediate) func.apply(context, args);
+      //     };
+      //     var callNow = immediate && !timeout;
+      //     clearTimeout(timeout);
+      //     timeout = setTimeout(later, wait);
+      //     if (callNow) func.apply(context, args);
+      //   };
+      // };
   
       const sliderImages = document.querySelectorAll('.slide-in');
   
+      let x = function(sliderImage){
+        var slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+        var imageBottom = sliderImage.offsetTop + sliderImage.height;
+        var isHalfShown = slideInAt > sliderImage.offsetTop;
+        var isNotScrolledPast = window.scrollY < imageBottom;
+        var direction = sliderImage.dataset.direction;
+
+        if (isHalfShown && isNotScrolledPast) {
+          sliderImage.classList.add(`slide-image-${direction}`);
+        } else {
+          sliderImage.classList.remove(`slide-image-${direction}`);
+        }
+      }
+
       function checkSlide() {
-        sliderImages.forEach(sliderImage => {
-          // half way through the image
-         
-          const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
-          console.log('slidein at - ', slideInAt)
-          // bottom of the image
-          const imageBottom = sliderImage.offsetTop + sliderImage.height;
-          console.log("image-bottom",imageBottom)
-          const isHalfShown = slideInAt > sliderImage.offsetTop;
-          const isNotScrolledPast = window.scrollY < imageBottom;
-          let direction = sliderImage.dataset.direction;
-          console.log('direction ',direction);
-          if (isHalfShown && isNotScrolledPast) {
-            sliderImage.classList.add(`slide-image-${direction}`);
-          } else {
-            sliderImage.classList.remove(`slide-image-${direction}`);
-          }
-        });
+        sliderImages.forEach(x);
       }
   
       window.addEventListener('scroll', checkSlide); 
