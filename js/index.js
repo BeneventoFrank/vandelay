f17e2890b0830f962db6f85b90059cec39df35e8
frankbenevento <fb.mentoslabs@gmail.com>
2018-09-30 09:41:31 -0700

    tweaking the layout for mobile

--- js/index.js
+++ js/index.js
@@ -1,40 +1,18 @@
 
+    var img = document.getElementById('img1');
+    var img2 = document.getElementById('img2');
+    var img3 = document.getElementById('img3');
 
-
-    if(!navigator.userAgent.toLowerCase().includes('ipad')){
-
-      var img = document.getElementById('img1');
+    if(navigator.userAgent.toLowerCase().includes('ipad')){
+      img2.classList.add('hidden');
+      img3.classList.add('hidden');
+    }
+    else 
+    { 
       img.classList.add('bgImage');
-
-      var img2 = document.getElementById('img2');
       img2.classList.add('bgImage');
-
-      var img3 = document.getElementById('img3');
       img3.classList.add('bgImage');
-  
-      
-      const sliderImages = document.querySelectorAll('.slide-in');
-  
-      var x = function(sliderImage){
-          var slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
-          var imageBottom = sliderImage.offsetTop + sliderImage.height;
-          var isHalfShown = slideInAt > sliderImage.offsetTop;
-          var isNotScrolledPast = window.scrollY < imageBottom;
-          var direction = sliderImage.dataset.direction;
-
-          if (isHalfShown && isNotScrolledPast) {
-            //sliderImage.classList.add(`slide-image-${direction}`);
-          } else {
-            //sliderImage.classList.remove(`slide-image-${direction}`);
-          }
-      }
-
-      function checkSlide() {
-        sliderImages.forEach(x);
-      }
-  
-      window.addEventListener('scroll', checkSlide); 
-  
     }
 
+    
 
